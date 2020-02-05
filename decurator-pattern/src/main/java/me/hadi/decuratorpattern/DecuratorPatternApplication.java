f7801003d577306b43bae4f6119f88f0fbaa529a
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2020-02-05 16:12:49 +0330

    initial project

---
+++ decurator-pattern/src/main/java/me/hadi/decuratorpattern/DecuratorPatternApplication.java
@@ -0,0 +1,13 @@
+package me.hadi.decuratorpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecuratorPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecuratorPatternApplication.class, args);
+    }
+
+}
---
+++ decurator-pattern/src/test/java/me/hadi/decuratorpattern/DecuratorPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hadi.decuratorpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecuratorPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
