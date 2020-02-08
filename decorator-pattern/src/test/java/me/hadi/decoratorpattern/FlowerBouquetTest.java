9f68d6b8ba999d11761310d16acbba9687369131
Hadi Jeddizahed <h.jeddizahed@tgbs.co>
2020-02-08 17:19:56 +0330

    initial project

--- decorator-pattern/src/test/java/me/hadi/decoratorpattern/FlowerBouquetTest.java
+++ decorator-pattern/src/test/java/me/hadi/decoratorpattern/FlowerBouquetTest.java
@@ -1,8 +1,12 @@
 package me.hadi.decoratorpattern;
 
 
+import me.hadi.decoratorpattern.service.flowerbouquet.FlowerBouquet;
+import me.hadi.decoratorpattern.service.flowerbouquet.OrchidBouquet;
 import me.hadi.decoratorpattern.service.flowerbouquet.RoseBouquet;
 import me.hadi.decoratorpattern.service.flowerbouquet.decorated.GlitterDecorate;
+import me.hadi.decoratorpattern.service.flowerbouquet.decorated.PaperWrapper;
+import me.hadi.decoratorpattern.service.flowerbouquet.decorated.RibbonBow;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,13 +15,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class FlowerBouquetTest {
 
-    private RoseBouquet roseBouquet;
+    private FlowerBouquet roseBouquet;
+    private FlowerBouquet orchidBouquet;
     private GlitterDecorate glitterDecorate;
+    private PaperWrapper paperWrapper;
+
+    private RibbonBow ribbonBow;
 
     @BeforeEach
     void init() {
         roseBouquet = new RoseBouquet();
+        orchidBouquet = new OrchidBouquet();
+
+
+        paperWrapper = new PaperWrapper(roseBouquet);
         glitterDecorate = new GlitterDecorate(roseBouquet);
+
+        ribbonBow = new RibbonBow(orchidBouquet);
     }
 
 
@@ -26,6 +40,22 @@ public class FlowerBouquetTest {
 
         double cost = glitterDecorate.cost();
         Assertions.assertEquals(16d, cost);
+
+    }
+
+    @Test
+    public void test_roseBouquet_with_paperWrapper() {
+
+        double cost = paperWrapper.cost();
+        Assertions.assertEquals(15d, cost);
+
+    }
+
+    @Test
+    public void test_orchidBouquet_with_ribbonRow() {
+
+        double cost = ribbonBow.cost();
+        Assertions.assertEquals(35.5, cost);
 
     }
 }
